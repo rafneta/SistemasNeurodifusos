ea6982aedc8abd6cabe6494f79353042894de53e
rafaMac-Air <raf_neta@hotmail.com>
2019-01-06 13:58:08 -0600

    Se agregaron 4 entregables
    Se agregaron  los primeros 4 entregables de la practica 1

--- Practicas/Practica2.m
+++ Practicas/Practica2.m
@@ -1,5 +1,5 @@
 %% Práctica 2: Implementación de sistemas de inferencia difusa 
-
+% Duración: 12 horas
 %% Objetivos 
 %%
 % 
--- Practicas/Practica3.m
+++ Practicas/Practica3.m
@@ -1,5 +1,5 @@
 %% Práctica 3: Aplicación de sitemas de inferencia difusa
-
+% Duración: 3 horas
 %% Objetivos 
 %%
 % 
